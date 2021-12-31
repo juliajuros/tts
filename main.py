737b83a1d7cba137dc1d89d4239748583b1d983c
kaszalotstefan <95172120+kaszalotstefan@users.noreply.github.com>
2021-12-31 17:14:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,50 @@
-import os
 from flask import Flask, jsonify, request
 import pyttsx3
 
-def tts(text):
+#from bitstring import BitArray
+
+#def binary(name):
+#    b=BitArray(bytes=open(name,'rb').read())
+    #with open('binary.txt', 'w') as file1:
+    #   file1.write(b.bin)
+#    return b.bin
+
+def opeen(file):
+    with open(file, encoding ='unicode_escape') as f:
+        data = f.read()
+    return data
+def tts(text, rate = 150, volume = 1):
     engine = pyttsx3.init()
-    engine.setProperty('rate', 150)
-    engine.setProperty('volume', 1)
-    engine.save_to_file(text, 'tekst.mp3')
+    engine.setProperty('rate', rate)
+    engine.setProperty('volume', volume)
+    #engine.save_to_file(text, 'tekst.wav')
+    engine.save_to_file(text, 'tekst.txt')
     engine.runAndWait()
-    return text
+    data = opeen("tekst.txt")
+    #bin = binary('tekst.wav')
+    return data
 
 app = Flask(__name__)
 @app.route('/')
 def home():
-    txt = 'stół z powyłamywanymi nogami, chrząszcz brzmi w trzcinie w szczebrzeszynie'
+    txt = 'stol z powylamywanymi nogami, chrzaszcz brzmi w trzcinie w szczebrzeszynie'
     return jsonify({"output-text": tts(txt)})
 @app.route('/json', methods=['POST'])
 def json_example():
     req_data = request.get_json()
     txt = req_data['text']
-    #alphabet = req_data['alphabet'] -nasze?
-    output = tts(txt)
+    volume = req_data['volume']
+    rate = req_data['rate']
+    output = tts(txt, rate, volume)
     return jsonify({"output-text": output})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port = 5000)
+
+#curl --header "Content-Type: application/json" \
+#  --request POST \
+#  --data '{"text":"dżdżownica i jeż"}' \
+#localhost:56733/json
 
 #curl --header "Content-Type: application/json"   --request POST   --data '{"text":"dżdżownica i jeż"}' localhost:5000/json
 
